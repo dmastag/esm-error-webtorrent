7ceb4ef77fb898db2bdf727c00fbd9b4cf51f06e
Julian Alimin <dmastag@yahoo.com>
2023-07-26 23:43:42 +0700

    Add Webtorrent

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import WebTorrent from 'webtorrent'
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return ({ message: 'Hello API' });
+    return { message: 'Hello API' };
+  }
+
+  startTorrent() {
+    const client = new WebTorrent();
+    const magnetURI = '...';
+
+    client.add(magnetURI, (torrent) => {
+      // Got torrent metadata!
+      console.log('Client is downloading:', torrent.infoHash);
+
+      for (const file of torrent.files) {
+        document.body.append(file.name);
+      }
+    });
   }
 }
